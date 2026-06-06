49c68aad32ebb00ed4bc68386baf9c931c1581e7
habibighufronhilmiy <habibighufron11@gmail.com>
2026-06-06 16:15:17 +0700

    Fix email not sending - add missing encryption config and remove Brevo code

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,11 +2,8 @@
 
 namespace App\Providers;
 
-use Illuminate\Support\Facades\Mail;
 use Illuminate\Support\Facades\URL;
 use Illuminate\Support\ServiceProvider;
-use Symfony\Component\Mailer\Bridge\Brevo\Transport\BrevoTransportFactory;
-use Symfony\Component\Mailer\Transport\Dsn;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -20,14 +17,5 @@ class AppServiceProvider extends ServiceProvider
         if ($this->app->environment('production')) {
             URL::forceScheme('https');
         }
-
-        Mail::extend('brevo', function (array $config) {
-            $factory = new BrevoTransportFactory(
-                dispatcher: $this->app->make(\Symfony\Component\EventDispatcher\EventDispatcherInterface::class),
-                logger: $this->app->make(\Psr\Log\LoggerInterface::class),
-            );
-
-            return $factory->create(Dsn::fromString($config['url']));
-        });
     }
 }
